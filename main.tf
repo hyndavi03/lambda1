2d7407df4ad00125e2ec6b937022a7f3fb5ea5aa
Hyndavi <venkatahyndavi@gmail.com>
2023-10-31 23:25:09 +0530

    pushed

--- main.tf
+++ main.tf
@@ -61,8 +61,8 @@ resource "aws_eip" "example" {}
 
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "./lambda_function.py"  # Path to the directory containing your Lambda code
-  output_path = "./lambda_function.zip"
+  source_dir  = "${path.module}/lambda_function.py"  # Path to the directory containing your Lambda code
+  output_path = "${path.module}/lambda_function.zip"
 }
 
 resource "aws_lambda_function" "example" {
